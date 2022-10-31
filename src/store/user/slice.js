7fd4d4c8317f2182c1ecae306366b928a49f6d99
prathima <kanadamprathima@gmail.com>
2022-10-31 21:50:36 +0100

    initialised store and added reduxnuser&pizza slices

---
+++ src/store/user/slice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = { name: "selva", id: 42 };
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+});
+export const {} = userSlice.actions;
+export default userSlice.reducer;
